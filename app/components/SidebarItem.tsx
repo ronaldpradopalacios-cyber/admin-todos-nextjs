d65c20b6628a8509ee8a0a54279a10d39fc02737
sauderx <ronaldpradopalacios@gmail.com>
2026-07-25 21:35:20 -0400

    Se agregan componentes para renderizar lostado de todos + conexion con prima en TodosGrid.tsx

--- app/components/SidebarItem.tsx
+++ app/components/SidebarItem.tsx
@@ -18,7 +18,7 @@ export const SidebarItem = ({ path, icon, title }: Props) => {
       <Link
         href={path}
         className={`
-          relative px-4 py-3 flex items-center space-x-4 rounded-xl  group
+          relative px-4 py-3 flex items-center space-x-4 rounded-xl group
           hover:bg-gradient-to-r hover:bg-sky-500 hover:text-white
           ${path === pathName ? "text-white bg-gradient-to-r from-sky-600 to-cyan-400" : ""}
         `}
--- app/dashboard/rest-todos/page.tsx
+++ app/dashboard/rest-todos/page.tsx
@@ -1,7 +1,24 @@
-export default function RestTodosPage() {
+import prisma from "@/app/lib/prisma";
+import { TodosGrid } from "@/app/todos/components/TodosGrid";
+
+export const metadata = {
+  title: "Listado de TODOS",
+  description: "SEO Title",
+};
+
+export default async function RestTodosPage() {
+  const todos = await prisma.todo.findMany({ orderBy: { title: "asc" } });
+
+  // useEffect(() => {
+  //   fetch("/api/todos")
+  //     .then((response) => response.json())
+  //     .then((todos) => console.log(todos));
+  // }, []);
+
   return (
     <div>
-      <h1>Rest Todos Page</h1>
+      {/* formulario para agregar todos... */}
+      <TodosGrid todos={todos} />
     </div>
   );
 }
